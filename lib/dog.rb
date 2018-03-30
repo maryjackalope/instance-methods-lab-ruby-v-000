39dc293effb2fd3f4bcbc24ba607985b75e86bb9
Mary Jack <maryjackinthebox@gmail.com>
2018-03-30 21:59:56 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,11 @@
 class Dog
   
-end
- 
-fido = Dog.new
- 
-snoopy = Dog.new
-
-lassie = Dog.new
+  def bark
+    puts "Woof!"
+  end 
+  
+  def sit 
+    puts "The Dog is sitting"
+  end
+  
+end #Dog
